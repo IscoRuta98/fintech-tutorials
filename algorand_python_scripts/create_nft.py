8f18e4d9fd047dc4489aa4deb5df9710b1f5bed0
IscoRuta98 <fmruta98@gmail.com>
2024-03-01 17:00:42 +0200

    refactor: addedd optin and transfer

--- algorand_python_scripts/create_nft.py
+++ algorand_python_scripts/create_nft.py
@@ -34,7 +34,7 @@ Optionally, retrieve the asset ID of the newly created NFT from the results dict
 
 from algosdk import account, mnemonic, transaction
 from algosdk.v2client import algod
-from decouple import config
+from typing import Dict, Any
 
 # Load the API key from the environment file
 api_key = ''
@@ -44,7 +44,7 @@ api_key = ''
 algod_address = "https://testnet-api.algonode.cloud"
 algod_client = algod.AlgodClient(api_key, algod_address)
 
-mn = 'ENTER MNEMONIC HERE'
+mn = 'jungle length act gentle cabin nothing obtain alpha gorilla mimic width diagram element tornado syrup budget diagram rose box pipe prepare gentle field about review'
 private_key = mnemonic.to_private_key(mn)
 # print(f"Base64 encoded private key: {private_key}")
 address = account.address_from_private_key(private_key)
@@ -57,15 +57,15 @@ txn = transaction.AssetConfigTxn(
     sender=address,
     sp=sp,
     default_frozen=False,
-    unit_name="ESG",
-    asset_name="ESG Token",
+    unit_name="DDOLAR",
+    asset_name="Digital Dollar",
     manager=address,
     reserve=address,
     freeze=address,
     clawback=address,
     url="https://www.weforum.org/reports/annual-report-2021-2022",
-    total=1,
-    decimals=0,
+    total=10000000,
+    decimals=2,
 )
 
 # Sign with secret key of creator
@@ -74,9 +74,53 @@ stxn = txn.sign(private_key)
 txid = algod_client.send_transaction(stxn)
 print(f"Sent asset create transaction with txid: {txid}")
 # # Wait for the transaction to be confirmed
-# results = transaction.wait_for_confirmation(algod_client, txid, 4)
-# print(f"Result confirmed in round: {results['confirmed-round']}")
+results = transaction.wait_for_confirmation(algod_client, txid, 4)
+print(f"Result confirmed in round: {results['confirmed-round']}")
 
-# # grab the asset id for the asset we just created
-# created_asset = results["asset-index"]
-# print(f"Asset ID created: {created_asset}")
+# grab the asset id for the asset we just created
+created_asset = results["asset-index"]
+print(f"Asset ID created: {created_asset}")
+
+# example: ASSET_INFO
+# Retrieve the asset info of the newly created asset
+asset_info = algod_client.asset_info(created_asset)
+asset_params: Dict[str, Any] = asset_info["params"]
+print(f"Asset Name: {asset_params['name']}")
+print(f"Asset params: {list(asset_params.keys())}")
+# example: ASSET_INFO
+
+
+# example: ASSET_OPTIN
+sp = algod_client.suggested_params()
+receiver_address = "CGI5X2I3LHDSNIZZOKUONPXK7KXPWJBZDYVO4VXJZRQPUHDW5XPPP3RKDY"
+# Create opt-in transaction
+# asset transfer from me to me for asset id we want to opt-in to with amt==0
+optin_txn = transaction.AssetOptInTxn(
+    sender="CGI5X2I3LHDSNIZZOKUONPXK7KXPWJBZDYVO4VXJZRQPUHDW5XPPP3RKDY", sp=sp, index=created_asset
+)
+signed_optin_txn = optin_txn.sign(private_key="wvJG12J4ZJ+OmpFVEbfbzG9eVqQC95TM/qfipzoPDeIRkdvpG1nHJqM5cqjmvur6rvskOR4q7lbpzGD6HHbt3g==")
+txid = algod_client.send_transaction(signed_optin_txn)
+print(f"Sent opt in transaction with txid: {txid}")
+
+# Wait for the transaction to be confirmed
+results = transaction.wait_for_confirmation(algod_client, txid, 4)
+print(f"Result confirmed in round: {results['confirmed-round']}")
+# example: ASSET_OPTIN
+
+# example: ASSET_XFER
+sp = algod_client.suggested_params()
+# Create transfer transaction
+xfer_txn = transaction.AssetTransferTxn(
+    sender=address,
+    sp=sp,
+    receiver=receiver_address,
+    amt=10000,
+    index=created_asset,
+)
+signed_xfer_txn = xfer_txn.sign(private_key=private_key)
+txid = algod_client.send_transaction(signed_xfer_txn)
+print(f"Sent transfer transaction with txid: {txid}")
+
+results = transaction.wait_for_confirmation(algod_client, txid, 4)
+print(f"Result confirmed in round: {results['confirmed-round']}")
+# example: ASSET_XFER
